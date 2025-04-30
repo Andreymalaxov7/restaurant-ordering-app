0a5d9e9ce271a469b02d80696916cd8f79585cc1
Demid <155105123+Andreymalaxov7@users.noreply.github.com>
2025-04-30 22:42:09 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
-import {menuArray} from "./data";
+const menuArray = [
+    {
+        name: "Pizza",
+        ingredients: ["pepperoni", "mushroom", "mozzarella"],
+        id: 0,
+        price: 14,
+        emoji: "🍕",
+        quantity: 0
+    },
+    {
+        name: "Hamburger",
+        ingredients: ["beef", "cheese", "lettuce"],
+        price: 12,
+        emoji: "🍔",
+        id: 1,
+        quantity: 0
+    },
+        {
+        name: "Beer",
+        ingredients: ["grain, hops, yeast, water"],
+        price: 10,
+        emoji: "🍺",
+        id: 2,
+        quantity: 0
+    }
+]
 
 // const jsPlusButton = document.querySelector(".js-plus-btn");
 const menuContainer = document.querySelector(".menu-container");
